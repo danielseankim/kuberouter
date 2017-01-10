64ff4d852aba52580445a8c3aa24e599e1a595cb
logankimmel <lkimmel@vmware.com>
2017-01-10 12:03:13 -0600

    Working dynamic reverse proxy

--- oxy.go
+++ oxy.go
@@ -1,23 +1,17 @@
 package main
 
 import (
+    "log"
     "net/http"
-    "net/url"
     "fmt"
     "os"
     "strings"
     "regexp"
-    "github.com/vulcand/oxy/forward"
+    "net/http/httputil"
 )
 
-type target struct {
-    Name string
-    Endpoint string
-    Port string
-}
 
-
-func getAddrs() ([]target) {
+func getAddrs() (map[string]string) {
     services := ParseRawServices(getEnviro())
     return services
 }
@@ -27,56 +21,49 @@ func getEnviro() []string {
 }
 //ParseRawServices returns a map of the services we need to proxy
 // expecs environment variabls like: OUTSCORE_DEPLOYMENT_PORT_8080_TCP_ADDR=10.95.249.177
-func ParseRawServices(env []string) []target {
-    serviceMap := []target{}
-
+func ParseRawServices(env []string) map[string]string {
+    targets := map[string]string{}
     for _, val := range env {
-        target := target{}
         if !strings.Contains(val, "ADDR") {
             continue
         }
-        target.Name = strings.Split(val, "_")[0]
         portre := regexp.MustCompile("_(\\d*)_")
         port := strings.Replace(portre.FindString(val), "_", "", 2)
         ipre := regexp.MustCompile("(?:[0-9]{1,3}\\.){3}[0-9]{1,3}")
-        target.Port = port
-        target.Endpoint = ipre.FindString(val)
-        serviceMap = append(serviceMap, target)
+        targets[port] = ipre.FindString(val)
     }
-    return serviceMap
+    return targets
 }
 
 func PortParse(url string) string {
      return strings.Split(url, ":")[1]
 }
 
-func main() {
+func NewMultipleHostReverseProxy(targets map[string]string) *httputil.ReverseProxy {
+    director := func(req *http.Request) {
+            requestPort := PortParse(req.Host)
+            newHost := fmt.Sprintf("%s:%s", targets[requestPort], requestPort)
+            req.URL.Scheme = "http"
+            req.URL.Host = newHost
+        }
+        return &httputil.ReverseProxy{Director: director}
+}
 
+func main() {
     targets := getAddrs()
     fmt.Printf("Targets found: %q\n", targets)
-    target_size := len(targets)
-    target_id := 1
-    rs := []http.HandlerFunc{}
-    for _, t := range targets {
-        fmt.Printf("Serving: %q:%q\n", t.Endpoint, t.Port)
-
-        fwd, _ := forward.New()
-        target := fmt.Sprintf("%s:%s", t.Endpoint, t.Port)
-        r := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-                req.URL = &url.URL{
-                    Scheme: "http",
-                    Host: target,
-                }
-                fwd.ServeHTTP(w, req)
-        })
-        fmt.Printf("%q", r)
-        rs = append(rs, r)
-        if target_id != target_size {
-            // go http.ListenAndServe(fmt.Sprintf(":%s", t.Port), &r)
+    proxy := NewMultipleHostReverseProxy(targets)
+    targetSize := len(targets)
+    targetID := 1
+    for port, _ := range targets {
+        serve := fmt.Sprintf(":%s", port)
+        if (targetID == targetSize) {
+            log.Fatal(http.ListenAndServe(serve, proxy))
         } else {
-            // http.ListenAndServe(fmt.Sprintf(":%s", t.Port), &r)
+            go func() {
+                log.Fatal(http.ListenAndServe(serve, proxy))
+            }()
         }
-        target_id = target_id + 1
-	}
-    fmt.Printf("%q", rs)
+        targetID = targetID + 1
+    }
 }
