08e377c5e47be3a9825b93db795a1728705436e3
logankimmel <lkimmel@vmware.com>
2017-02-16 10:36:44 -0600

    rename to kuberouter

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-Dynamic Routing with Oxy
+# Dynamic Routing of K8S Containers
 
-This uses the oxy router from volcand to create servers
+This uses the net/http router to create servers
 from environment variables.
-Expects environment variabls like: OUTSCORE_DEPLOYMENT_PORT_8080_TCP_ADDR=10.95.249.177
+Expects environment variables like: OUTSCORE_DEPLOYMENT_PORT_8080_TCP_ADDR=10.95.249.177
+
+When running in K8S these variables will be created by default.
+
+For example, the environment variable `OUTSCORE_DEPLOYMENT_PORT_8080_TCP_ADDR=10.95.249.177` will
+create a reverse proxy on port 8080 to 10.95.249.177
---
+++ kuberoute.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func getAddrs() map[string]string {
+	services := ParseRawServices(getEnviro())
+	return services
+}
+
+func getEnviro() []string {
+	return os.Environ()
+}
+
+//ParseRawServices returns a map of the services we need to proxy
+// expecs environment variabls like: OUTSCORE_DEPLOYMENT_PORT_8080_TCP_ADDR=10.95.249.177
+func ParseRawServices(env []string) map[string]string {
+	targets := map[string]string{}
+	for _, val := range env {
+		if !strings.Contains(val, "ADDR") {
+			continue
+		}
+		if strings.Contains(val, "ROUTER") || strings.Contains(val, "KUBERNETES") {
+			continue
+		}
+		fmt.Println("%s", val)
+		portre := regexp.MustCompile("_(\\d*)_")
+		port := strings.Replace(portre.FindString(val), "_", "", 2)
+		ipre := regexp.MustCompile("(?:[0-9]{1,3}\\.){3}[0-9]{1,3}")
+		portI, _ := strconv.Atoi(port)
+		if portI > 65535 {
+			continue
+		}
+		targets[port] = ipre.FindString(val)
+	}
+	return targets
+}
+
+func PortParse(url string) string {
+	return strings.Split(url, ":")[1]
+}
+
+func NewMultipleHostReverseProxy(targets map[string]string) *httputil.ReverseProxy {
+	director := func(req *http.Request) {
+		requestPort := PortParse(req.Host)
+		newHost := fmt.Sprintf("%s:%s", targets[requestPort], requestPort)
+		req.URL.Scheme = "http"
+		req.URL.Host = newHost
+	}
+	return &httputil.ReverseProxy{Director: director}
+}
+
+func main() {
+	targets := getAddrs()
+	fmt.Printf("Targets found: %q\n", targets)
+	proxy := NewMultipleHostReverseProxy(targets)
+	targetSize := len(targets)
+	targetID := 1
+	for port := range targets {
+		serve := fmt.Sprintf(":%s", port)
+		if targetID == targetSize {
+			http.ListenAndServe(serve, proxy)
+		} else {
+			go func() {
+				http.ListenAndServe(serve, proxy)
+			}()
+		}
+		targetID = targetID + 1
+	}
+}
